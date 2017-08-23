6b9ce260dee4fab95b54efa05b2e26d7fe2a092f
Alejandro Morales <alexmorales30@gmail.com>
2017-08-23 13:42:05 -0400

    Added date picker component and have it set to filter events. The component starts off displaying events for the current date.

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,16 +1,17 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, DoCheck, AfterContentChecked} from '@angular/core';
 import { CalendarDataService } from '../shared/services/calendar-data.service';
 import { MDCEvent } from '../shared/models/MDCEvent';
 import {environment} from '../../environments/environment';
 import { DatePickerOptions, DateModel } from 'ng2-datepicker';
 import * as _ from 'lodash';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent implements OnInit {
+export class EventsComponent implements OnInit, DoCheck, AfterContentChecked {
 
   public events: Array<MDCEvent> = [];
   public filteredArray: Array<MDCEvent> = [];
@@ -24,9 +25,18 @@ export class EventsComponent implements OnInit {
   }
 
   ngOnInit() {
+    // Set current date as default value for ng2-datepicker.
+    const dateModel: DateModel = new DateModel();
+    const momentObj = moment();
+    dateModel.momentObj = momentObj;
+    dateModel.formatted = momentObj.format('l');
+    this.datePickerDate = dateModel;
 
+    // Get events from service in order to populate event list.
     this.calendarDataService.getEvents().subscribe((events: Array<MDCEvent>) => {
         this.events = events;
+
+        // Using filteredArray so that the view will automatically update when a new date is selected.
         this.filteredArray = events;
       },
       error => {
@@ -39,7 +49,19 @@ export class EventsComponent implements OnInit {
     }
 
   }
+
+  ngAfterContentChecked(){
+    this.applyFilter(this.datePickerDate.momentObj.format('YYYY-MM-DD'));
+  };
+
+  ngDoCheck(){
+    this.applyFilter(this.datePickerDate.momentObj.format('YYYY-MM-DD').toString());
+  }
+
   private applyFilter(filter: string){
+    if (filter === '')
+      return true;
+
     this.filteredArray = this.events.filter(item => {
       if (item.eventDate.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1){
         return true;
