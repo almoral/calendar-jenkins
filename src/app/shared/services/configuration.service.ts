24c43a1e5694c6c2548631cf13614bc993168f26
Alejandro Morales <alexmorales30@gmail.com>
2017-09-27 16:21:45 -0400

    Updating code to point at new endpoint.

--- src/app/shared/services/configuration.service.ts
+++ src/app/shared/services/configuration.service.ts
@@ -3,13 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigurationService {
 
-  // urlMayor: string = '/api/Mayor';
-  urlMayor = '/api/calendars/Mayor/events';
-  urlProof = '/api/ProofOne';
+  // urlMayor = '/api/calendars/Mayor/events';
+  urlDevCalendar = '/api/calendars/CalProof1/events';
   urlCategories = 'https://s0144821.miamidade.gov:7000/registration/api/topics';
   urlEvents = 'https://private-2b2f8b-calendar31.apiary-mock.com/api/calendars/id/events?from=&to=';
-  // urlCategories: string ='/categories';
-  // urlMayor: string = 'http://s0144821.miamidade.gov:7009/msgraph/api/calendars/Mayor';
+
   constructor() {
   }
 
