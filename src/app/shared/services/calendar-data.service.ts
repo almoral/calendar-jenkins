815639436547e03e477c658f6cc1687a3de0af77
Jorge Fiallega <jorgefiallega@gmail.com>
2017-11-05 08:25:48 -0500

    rewrote getEventsOnCalendar to handle 404 errors and use the httpClient. Refactor fromJSONArray moving it to the mdc-event.

--- src/app/shared/services/calendar-data.service.ts
+++ src/app/shared/services/calendar-data.service.ts
@@ -1,17 +1,25 @@
-import { Injectable } from '@angular/core';
-import { ConfigurationService } from './configuration.service';
-import 'rxjs/Rx';
-import * as _ from 'lodash';
-import {Http, Response, Headers} from '@angular/http';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {ConfigurationService} from "./configuration.service";
+import {Observable} from "rxjs";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 import {MdcEvent} from "../models/mdc-event";
+import * as _ from "lodash";
+import {Response} from "@angular/http";
+
+
+/**
+ * CalendarDataService is all about getting data
+ * for the calendar from external source. More specifically
+ * it encapsulates the http calls and mapping
+ * to the model of the application.
+ */
 
 @Injectable()
 export class CalendarDataService {
 
   constructor(
-//    private http: Http,
     private httpClient: HttpClient,
     private configurationService: ConfigurationService
   ) { }
@@ -21,17 +29,41 @@ export class CalendarDataService {
    * getEventsOnCalendar will get all the events belonging to a calendar
    * with id calendarId.
    * @param calendarId - The id of the calendar
-   * @returns {null}
+   * @returns {Observable<MdcEvent []>} - Success Observable array of MdcEvents
+   * http 404 - No calendar found: Observable empty array.
+   * Any other http error code will throw an observable exception.
+   *
    */
-  getEventsOnCalendar(calendarId: string) {//:Observable<MdcEvent[]>{
+  getEventsOnCalendar(calendarId: string): Observable<MdcEvent []> {
 
-    let url:string = '/api/calendar/${calendarId}/events';
+    let url: string = `/api/calendar/${calendarId}/events`;
 
-    this.httpClient.get(url).subscribe((data) =>{
-      console.log(data['results']);
-    });
+    return this.httpClient.get(url)
+      .map((rawEvents, index) => {
+        let events = MdcEvent.fromJSONArray(rawEvents['events']);
+        return events;
+      }).catch((err: HttpErrorResponse) => {
+        if (err.error instanceof ErrorEvent) {
+          // A client-side or network error occurred.
+          console.error('An error occurred:', err.error.message, err.error);
+          return Observable.throw('client-side or network error occurred.');
+        } else {
+          // The backend returned an unsuccessful response code.
+          switch (err.status) {
+            case 404: {
+              return Observable.of([]);
+            }
+            default: {
+              console.log(`Error returned code ${err.status}, body was: ${err.error}`);
+              return Observable.throw('Error ocurred: ' + err.status);
+            }
+          }
+        }
+      });
 
   }
+
+
 
   // getEventsOnCalendar(calendarId: string):Observable<MdcEvent[]>{
   //
@@ -61,38 +93,6 @@ export class CalendarDataService {
     //     console.error('ERROR: ', error);
     //     return error;
     //   });
-  }
-
-
-
-  /**
-   * maps the raw array of json languages into an array
-   * of Language Objects. If any of the elements in the
-   * array can not be mapped, it is ignored.
-   * @param response - Response object containing a body equal to json array representing raw languages
-   * @returns {Array<Language>} Converted response into an array
-   * of Language Objects. It will return an empty array if nothing
-   * in the response could be converted to a Language or the response hand an empty body.
-   */
-  private jsonToEvents = (response: Response) => {
-    if (_.isEmpty(response.json())) {
-      return null;
-    }
-
-    const raw: Array<any> = response.json().events;
-    const model: Array<MdcEvent> = raw.reduce(function (accumulator, item) {
-      const event = MdcEvent.fromJSON(item);
-      if (event) {
-        accumulator.push(event);
-      }
-      return accumulator;
-    }, []);
-
-    if (model.length === 0) {
-      return null;
-    }
-
-    return model;
   }
 
 
