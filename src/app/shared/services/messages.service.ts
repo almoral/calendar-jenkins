23d805258175ff17d33a825afd1dfbd145a9958d
Jorge Fiallega <jorgefiallega@gmail.com>
2017-11-05 16:23:32 -0500

    added message service and component to handle the display of messages in the application. Missing the styles.

---
+++ src/app/shared/services/messages.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
+
+@Injectable()
+export class MessagesService {
+
+
+  private errorsSubject = new BehaviorSubject<string[]>([]);
+
+  /**
+   * errors$ is the observable to subscribe to in order to
+   * be notified when the list of available errors change.
+   * @type {"../../Observable".Observable<T>}
+   */
+  errors$: Observable<string[]> = this.errorsSubject.asObservable();
+
+
+  constructor() {
+  }
+
+  /**
+   * error stores the errors and emits them to the subscribed observers.
+   * @param errors - the array list of errors to be saved and emitted
+   * to the observers looking for changes on the list of errors.
+   */
+  error(...errors:string[]) {
+    this.errorsSubject.next(errors);
+  }
+
+
+}
