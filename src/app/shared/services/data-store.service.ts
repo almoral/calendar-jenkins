98ffd4ec64e554a42ce6656cb9c8f5b00c7e0de8
Alex Morales <almoral>
2017-11-07 15:36:31 -0500

    Replace double quotes with single quotes.
    Replaced "rxjs" with "rxjs/Rx" for the import statement for BehaviorSubject and Observable.

--- src/app/shared/services/data-store.service.ts
+++ src/app/shared/services/data-store.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Observable} from "rxjs";
-import {MdcEvent} from "../models/mdc-event";
+import {BehaviorSubject, Observable} from 'rxjs/Rx';
+import {MdcEvent} from '../models/mdc-event';
 import * as _ from 'lodash';
-import {testEvents} from "../models/test-events";
+import {testEvents} from '../models/test-events';
 
 
 
