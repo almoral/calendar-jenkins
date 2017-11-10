bb0788b2ce96b166a06b300bc98b6d6a92165a79
Jorge Fiallega <jorgefiallega@gmail.com>
2017-11-10 18:18:23 -0500

    tests for data-store initializeEvents

--- src/app/shared/services/data-store.service.spec.ts
+++ src/app/shared/services/data-store.service.spec.ts
@@ -1,18 +1,17 @@
-import { TestBed, inject } from '@angular/core/testing';
-
-import { DataStoreService } from './data-store.service';
+import {TestBed, inject} from "@angular/core/testing";
+import {DataStoreService} from "./data-store.service";
 import {TestEvents} from "../models/test-events";
 import {EventService} from "./event.service";
 import {EventDataService} from "./event-data.service";
-import {HttpClient} from "@angular/common/http";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {ConfigurationService} from "./configuration.service";
+import {MdcEvent, MdcEventsByDate} from "../models/mdc-event";
 
 describe('DataStoreService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [DataStoreService, EventService, EventDataService, ConfigurationService],
-      imports:[HttpClientTestingModule]
+      imports: [HttpClientTestingModule]
     });
   });
 
@@ -20,11 +19,28 @@ describe('DataStoreService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('when calling initializeEvents the new events are emitted', inject([DataStoreService], (service: DataStoreService) => {
-    let events$ = service.events$
-    service.initializeEvents(TestEvents.testEventsOne)
+  it('when calling initializeEvents the new collection of events are emitted', inject([DataStoreService], (service: DataStoreService) => {
 
-    events$.subscribe(events => expect(events.length).toBe(3))
+    service.initializeEvents(TestEvents.testEventsTwo)
+    let events$ = service.events$
+    events$.subscribe(events => {
+        expect(events.length).toBe(2);
+        expect(events[0]).toEqual(jasmine.any(MdcEvent));
+        expect(events[1]).toEqual(jasmine.any(MdcEvent));
+      }
+    )
+  }));
+
+  it('when calling initializeEvents the collection grouped by date are emitted', inject([DataStoreService], (service: DataStoreService) => {
+
+    service.initializeEvents(TestEvents.testEventsTwo)
+    let eventsByDate$ = service.eventsByDate$
+    eventsByDate$.subscribe(events => {
+        expect(events.length).toBe(2);
+        expect(events[0]).toEqual(jasmine.any(MdcEventsByDate));
+        expect(events[1]).toEqual(jasmine.any(MdcEventsByDate));
+      }
+    )
   }));
 
 });
