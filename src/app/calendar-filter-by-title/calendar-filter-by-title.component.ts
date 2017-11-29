5b3b8f99f872b8b159efcb88b6d016f853646628
Jorge Fiallega <jorgefiallega@gmail.com>
2017-11-28 23:12:57 -0500

    cleaned up calendar-filter-by-title component

--- src/app/calendar-filter-by-title/calendar-filter-by-title.component.ts
+++ src/app/calendar-filter-by-title/calendar-filter-by-title.component.ts
@@ -1,25 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from "rxjs";
 import {DataStoreService} from "../shared/services/data-store.service";
 
 @Component({
-  selector: 'calendar-filter-by-title',
+  selector: 'mdc-calendar-filter-by-title',
   templateUrl: 'calendar-filter-by-title.component.html',
   styleUrls: ['calendar-filter-by-title.component.css']
 })
 export class CalendarFilterByTitleComponent implements OnInit {
 
-  title$: Observable<string>;
-  title:string = '';
-
-  constructor(private dataStoreService:DataStoreService ) {}
-
-  ngOnInit() {
-
-    this.title$ = Observable.of("Hey");
+  constructor(private dataStoreService:DataStoreService ) {
   }
 
-  onEnter(value: string) {this.dataStoreService.setTitle(value) }
+  ngOnInit() {
+  }
+
+  onKeyUp(value: string) {
+    this.dataStoreService.setTitle(value);
+  }
 
 
 }
