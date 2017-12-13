06a01303048697cf0fe93a0855fbe945b1379230
Alejandro Morales <alexmorales30@gmail.com>
2017-12-13 15:33:46 -0500

    Simplified the configuration and set an initial value for the currentPage option.

--- src/app/calendar-event-list/calendar-event-list.component.ts
+++ src/app/calendar-event-list/calendar-event-list.component.ts
@@ -11,6 +11,7 @@ import {Observable} from "rxjs";
 export class CalendarEventListComponent implements OnInit {
 
   events$: Observable<MdcEventsByDate[]>;
+  p = 1;
 
   constructor(private dataStore: DataStoreService) { }
 
