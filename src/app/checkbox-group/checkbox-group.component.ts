cfc084c21f9c3268b86b26a3b64bec853e910785
Alejandro Morales <alexmorales30@gmail.com>
2017-12-14 09:36:56 -0500

    Cleaned up the checkbox-group component to remove any application specific pieces.

--- src/app/checkbox-group/checkbox-group.component.ts
+++ src/app/checkbox-group/checkbox-group.component.ts
@@ -1,6 +1,4 @@
-import {Component, EventEmitter, forwardRef, Input, Output} from '@angular/core';
-import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
-import * as _ from 'lodash';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {DataStoreService} from '../shared/services/data-store.service';
 
 
@@ -8,53 +6,15 @@ import {DataStoreService} from '../shared/services/data-store.service';
 @Component({
     selector: 'mdc-checkbox-group',
     templateUrl: './checkbox-group.component.html',
-    styleUrls: ['./checkbox-group.component.css'],
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => CheckboxGroupComponent),
-            multi: true
-        }
-    ]
+    styleUrls: ['./checkbox-group.component.css']
 })
-export class CheckboxGroupComponent implements ControlValueAccessor {
-
+export class CheckboxGroupComponent  {
 
   @Input() optionsData = [];
-
-
   @Output() filter: EventEmitter<any> = new EventEmitter<any>();
 
-    // array of selected options to be pushed back to formGroup ---
-    selOptions = [];
-    channelArray = [];
-    propagateChange = ((_: any) => {});
 
     constructor(private dataStoreService: DataStoreService) {
-    }
-
-    objectCleanUp (id, channel) {
-        return {'id': id, 'channels': channel }
-    }
-
-    checkboxGroupChange(val) {
-
-        this.selOptions = [];
-        this.optionsData.forEach((item, index) => {
-            if (item.checked === true) {
-
-                this.selOptions.push(this.objectCleanUp(item. id, this.channelArray));
-            }
-
-        });
-
-        // push values out to formGroup ---
-        this.propagateChange(this.selOptions);
-
-    }
-
-    // control value assessor interface ---
-    writeValue(values: any) {
     }
 
     public filterEvents(filterValue: string) {
@@ -63,11 +23,5 @@ export class CheckboxGroupComponent implements ControlValueAccessor {
 
     }
 
-    registerOnChange(fn) {
-        this.propagateChange = fn;
-    }
-
-    registerOnTouched() {
-    }
 
 }
