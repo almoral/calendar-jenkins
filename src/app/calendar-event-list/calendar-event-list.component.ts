3f6c3a561047484b1ad1b755a1a4f65a6021504e
Alex Morales <almoral>
2017-12-20 10:08:25 -0500

    Added message to display when no events are showing.

--- src/app/calendar-event-list/calendar-event-list.component.ts
+++ src/app/calendar-event-list/calendar-event-list.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import {DataStoreService} from "../shared/services/data-store.service";
-import {MdcEvent, MdcEventsByDate} from "../shared/models/mdc-event";
-import {Observable} from "rxjs";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {DataStoreService} from '../shared/services/data-store.service';
+import {MdcEvent, MdcEventsByDate} from '../shared/models/mdc-event';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'mdc-calendar-event-list',
   templateUrl: './calendar-event-list.component.html',
   styleUrls: ['./calendar-event-list.component.css']
 })
-export class CalendarEventListComponent implements OnInit {
+export class CalendarEventListComponent implements OnInit, OnDestroy {
 
   events$: Observable<MdcEventsByDate[]>;
+  eventsLength = null;
+  eventSubscription: Subscription;
 
   constructor(private dataStore: DataStoreService) { }
 
   ngOnInit() {
-
     this.events$ = this.dataStore.eventsByDate$;
+    this.eventSubscription = this.events$.delay(600).subscribe( event => this.eventsLength = event.length);
+  }
 
+  ngOnDestroy() {
+    this.eventSubscription.unsubscribe();
   }
 }
