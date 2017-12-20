99dfa7283bab56b3e71656972b9bc4edd44453ce
Alejandro Morales <alexmorales30@gmail.com>
2017-12-20 14:30:18 -0500

    Cleaned up the routing module.

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,25 +5,12 @@ import {CalendarComponent} from '../calendar/calendar.component';
 import {CalendarEventDetailsComponent} from '../calendar-event-details/calendar-event-details.component';
 
 const routes: Routes = [
-  // {path: '', redirectTo: 'calendar', pathMatch: 'full' },
   {
     path: 'calendar',
     component: CalendarComponent
   },
-    // children: [
-      // {path: 'events', redirectTo: 'events', pathMatch: 'full'},
-      {path: 'calendar/event/:eventID', component: CalendarEventDetailsComponent}
-      ,
-    // ]
-  // }
-  // ,
-  // {path: '', component: CalendarComponent},
+  {path: 'calendar/event/:eventID', component: CalendarEventDetailsComponent},
   {path: '**', component: CalendarComponent}
-  // {path: 'event', component: CalendarEventDetailsComponent}
-  // {path: '', redirectTo: 'events', pathMatch: 'full'},
-  // {path: 'events', component: EventsComponent},
-  // {path: 'test', component: TestViewComponent},
-  // {path: '**', component: EventsComponent}
 ];
 
 @NgModule({
