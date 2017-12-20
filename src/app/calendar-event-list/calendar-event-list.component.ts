4192940d071547d777fe9b00d071998eb17160c0
Alejandro Morales <alexmorales30@gmail.com>
2017-12-20 10:31:06 -0500

    Simplified the check.

--- src/app/calendar-event-list/calendar-event-list.component.ts
+++ src/app/calendar-event-list/calendar-event-list.component.ts
@@ -1,28 +1,21 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataStoreService} from '../shared/services/data-store.service';
-import {MdcEvent, MdcEventsByDate} from '../shared/models/mdc-event';
+import {MdcEventsByDate} from '../shared/models/mdc-event';
 import {Observable} from 'rxjs/Observable';
-import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'mdc-calendar-event-list',
   templateUrl: './calendar-event-list.component.html',
   styleUrls: ['./calendar-event-list.component.css']
 })
-export class CalendarEventListComponent implements OnInit, OnDestroy {
+export class CalendarEventListComponent implements OnInit {
 
   events$: Observable<MdcEventsByDate[]>;
-  eventsLength = null;
-  eventSubscription: Subscription;
 
   constructor(private dataStore: DataStoreService) { }
 
   ngOnInit() {
     this.events$ = this.dataStore.eventsByDate$;
-    this.eventSubscription = this.events$.delay(600).subscribe( event => this.eventsLength = event.length);
   }
 
-  ngOnDestroy() {
-    this.eventSubscription.unsubscribe();
-  }
 }
