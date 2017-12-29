9d7b46cd29feff546f7fe50d69d814a72b5148f4
Alejandro Morales <alexmorales30@gmail.com>
2017-12-29 09:05:08 -0500

    Creating filtering service to handle ‘see results’ button click.

--- src/app/shared/services/filter.service.ts
+++ src/app/shared/services/filter.service.ts
@@ -1,8 +1,74 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {DateService} from './date.service';
+import * as _ from 'lodash';
+import {DataStoreService} from './data-store.service';
 
 @Injectable()
 export class FilterService {
 
-  constructor() { }
+  constructor(private dateService: DateService,
+              private dataStoreService: DataStoreService) { }
+
+  private year = new BehaviorSubject<string>(null);
+  private month = new BehaviorSubject<string>(null);
+  private day = new BehaviorSubject<string>(null);
+  private title = new BehaviorSubject<string>(null);
+
+
+  selectedCategories: Array<string> = [];
+
+  // Creating observables as getters to keep the subjects private.
+  year$ = this.year.asObservable();
+  month$ = this.month.asObservable();
+  day$ = this.day.asObservable();
+  title$ = this.title.asObservable();
+
+  setYear(value) {
+    this.year.next(value);
+  }
+
+  setMonth(value) {
+    this.month.next(value);
+  }
+
+  setDay(value) {
+    this.day.next(value);
+  }
+
+  setTitle(value) {
+    this.title.next(value);
+  }
+
+
+  public filterCategories(selection: string) {
+
+    if ( _.indexOf(this.selectedCategories, selection) === -1) {
+      this.selectedCategories.push(selection);
+    } else {
+      _.remove(this.selectedCategories, category => category === selection);
+    }
+
+    this.dataStoreService.setCategoriesFilter(this.selectedCategories);
+  }
+
+  public filterEventsByDate() {
+
+    const year = this.year.getValue();
+    const month = this.month.getValue();
+    const day = this.day.getValue();
+
+    if (day === '' && month === '') {
+      this.dateService.filterByYear(year);
+    }
+    if (month !== '' && day === '') {
+      this.dateService.filterByMonth(year, month);
+    }
+    if (day !== '' && month !== '') {
+      this.dateService.filterByDate(year, month, day);
+    }
+  }
+
+
 
 }
