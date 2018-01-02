0c5c502842f7332482e04b1085a34204825ca284
Alex Morales <almoral>
2018-01-02 08:55:03 -0500

    Adding code for date filtering to branch.

---
+++ src/app/shared/services/filter.service.ts
@@ -0,0 +1,50 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {DateService} from './date.service';
+
+
+@Injectable()
+export class FilterService {
+
+  constructor(private dateService: DateService) { }
+
+  private year = new BehaviorSubject<string>(null);
+  private month = new BehaviorSubject<string>(null);
+  private day = new BehaviorSubject<string>(null);
+
+
+  // Creating observables as getters to keep the subjects private.
+  year$ = this.year.asObservable();
+  month$ = this.month.asObservable();
+  day$ = this.day.asObservable();
+
+  setYear(value) {
+    this.year.next(value);
+  }
+
+  setMonth(value) {
+    this.month.next(value);
+  }
+
+  setDay(value) {
+    this.day.next(value);
+  }
+
+  public filterEventsByDate() {
+
+    const year = this.year.getValue();
+    const month = this.month.getValue();
+    const day = this.day.getValue();
+
+    if (day === '' && month === '') {
+      this.dateService.filterByYear(year);
+    }
+    if (month !== '' && day === '') {
+      this.dateService.filterByMonth(year, month);
+    }
+    if (day !== '' && month !== '') {
+      this.dateService.filterByDate(year, month, day);
+    }
+  }
+
+}
