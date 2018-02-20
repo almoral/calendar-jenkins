a98a48c06473e1c8d08db6c7da68a8f05f92b6e7
Alejandro Morales <alexmorales30@gmail.com>
2018-02-20 18:21:48 -0500

    Got the message working correctly with a simple flag.

--- src/app/calendar-event-list/calendar-event-list.component.ts
+++ src/app/calendar-event-list/calendar-event-list.component.ts
@@ -2,9 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {DataStoreService} from '../shared/services/data-store.service';
 import {MdcEventsByDate} from '../shared/models/mdc-event';
 import {Observable} from 'rxjs/Observable';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {Subject} from 'rxjs/Subject';
-import {share} from 'rxjs/operators';
 
 
 @Component({
@@ -15,33 +12,18 @@ import {share} from 'rxjs/operators';
 export class CalendarEventListComponent implements OnInit {
 
   events$: Observable<MdcEventsByDate[]>;
-  eventsSubject = new Subject<MdcEventsByDate[]>();
   page = 1;
   eventsAreLoading = true;
 
   constructor(private dataStore: DataStoreService) { }
 
   ngOnInit() {
-
-    const sharedEvents = this.dataStore.eventsByDate$.pipe(share());
-
-    this.events$ = sharedEvents;
-
-    sharedEvents.subscribe( events => {
-      this.eventsSubject.next(events);
-    });
-
-    this.eventsSubject.subscribe( events => {
+    this.dataStore.eventsByDate$.subscribe( events => {
+      this.events$ = Observable.of(events);
       if (events.length > 0) {
-        console.log('events date in events subject: ', events);
-        this.eventsSubject.complete();
+        this.eventsAreLoading = false;
       }
-    },
-      error => console.log('error: ', error),
-      () => {
-      console.log('***************Events Subject completed!!');
-      this.eventsAreLoading = false;
-      });
+    });
 
   }
 
