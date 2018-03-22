884a28950d09fc85c1cdbf4f8e861eaa4e1ed898
Alex Morales <almoral>
2018-03-22 09:13:12 -0400

    WIP: Implementing modal dialog for event details.

--- src/app/calendar-grid-event/calendar-grid-event.component.ts
+++ src/app/calendar-grid-event/calendar-grid-event.component.ts
@@ -1,12 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {MdcEvent} from '../shared/models/mdc-event';
+import {IModalDialog} from 'ngx-modal-dialog';
 
 @Component({
   selector: 'mdc-calendar-grid-event',
   templateUrl: './calendar-grid-event.component.html',
   styleUrls: ['./calendar-grid-event.component.css']
 })
-export class CalendarGridEventComponent implements OnInit {
+export class CalendarGridEventComponent implements OnInit, IModalDialog {
 
   @ Input()
   event: MdcEvent;
@@ -16,5 +17,11 @@ export class CalendarGridEventComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  dialogInit() {}
+
+  // openDialog() {}
+
+
 
 }
