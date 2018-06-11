64d6cfbfe2027d93f469be86b4b62ec08d6f19c2
Alex Morales <almoral>
2018-06-11 08:52:19 -0400

    WIP: Getting the right hand side component to work correctly.

--- src/app/calendar-event-list/calendar-event-list.component.ts
+++ src/app/calendar-event-list/calendar-event-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {DataStoreService} from '../shared/services/data-store.service';
 import {MdcEventsByDate} from '../shared/models/mdc-event';
 import {Observable} from 'rxjs/Observable';
+import {environment} from '../../environments/environment';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'mdc-calendar-event-list',
@@ -11,12 +13,21 @@ import {Observable} from 'rxjs/Observable';
 export class CalendarEventListComponent implements OnInit {
 
   events$: Observable<MdcEventsByDate[]>;
+  displayFullView = environment.displayFullView;
   page = 1;
 
   constructor(private dataStore: DataStoreService) { }
 
   ngOnInit() {
-    this.events$ = this.dataStore.eventsByDate$;
+
+    if ( !this.displayFullView ) {
+      this.events$ = this.dataStore.eventsByDate$
+        .pipe(
+          take(5)
+        );
+    } else {
+      this.events$ = this.dataStore.eventsByDate$;
+    }
   }
 
 
