5190b6055495d42c58c4ae6c506d636c081112b7
Alejandro Morales <alexmorales30@gmail.com>
2018-06-18 10:06:44 -0400

    WIP: Handling printing for individual events.
    Created window service to access the native window object.
    Added it as a provider in the app module.

--- src/app/calendar-event/calendar-event.component.ts
+++ src/app/calendar-event/calendar-event.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit, Input} from '@angular/core';
 import {MdcEvent} from '../shared/models/mdc-event';
 import * as $ from 'jquery';
 import {environment} from '../../environments/environment';
+import {WindowRef} from '../shared/services/window-ref.service';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {delay, skip} from 'rxjs/operators';
 
 @ Component({
   selector: 'mdc-calendar-event',
@@ -13,27 +16,47 @@ export class CalendarEventComponent implements OnInit {
   @ Input()
   event: MdcEvent;
   displayFullView = environment.displayFullView;
+  printItem = new BehaviorSubject<boolean>(false);
+  $printItem = this.printItem.asObservable();
 
-  constructor() { }
+  constructor(private winRef: WindowRef) { }
 
   ngOnInit() {
+
+    this.$printItem
+      .pipe(
+        skip(1),
+        delay(1000)
+      )
+      .subscribe( value => {
+
+        if (value) {
+          this.winRef.nativeWindow.print();
+        }
+      });
+
   }
 
-  public showHide(event): void{
+  public showHide(event): void {
 
     $(event.currentTarget).next().slideToggle();
     $(event.currentTarget).toggleClass('active');
 
     // For ADA Compliance
-    var expanded = $(event.currentTarget).next().attr('aria-expanded');
+    let expanded = $(event.currentTarget).next().attr('aria-expanded');
 
     if (expanded === 'true') {
       expanded = 'false';
+      this.printItem .next(false);
     } else {
       expanded  = 'true';
-    };
+    }
 
     $(event.currentTarget).next().attr('aria-expanded', expanded);
+  }
+
+  public printEvent(): void {
+    this.printItem.next(true);
   }
 
 }
